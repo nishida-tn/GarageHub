f29269a5a3b47e11f3229ec5f12d2f5a29a079d5
Thales Nishida <nishida.tn@gmail.com>
2026-03-10 17:51:23 -0300

    Update the `GarageHubTheme` to default to dark mode.
    Specific changes:
    - Modified `GarageHubTheme` in `Theme.kt` to set `darkTheme` to `true` by default, replacing the system setting check.

--- app/src/main/java/com/hsgaragepecas/garagehub/ui/theme/Theme.kt
+++ app/src/main/java/com/hsgaragepecas/garagehub/ui/theme/Theme.kt
@@ -42,7 +42,7 @@ private val LightColorScheme = lightColorScheme(
  */
 @Composable
 fun GarageHubTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = true,
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
